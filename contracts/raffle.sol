353a1bbc0e88399f298144ca8a139babe3fa98c5
kazem <kazem_abdi@yahoo.com>
2022-09-09 16:07:50 +0200

    all

--- contracts/raffle.sol
+++ contracts/raffle.sol
@@ -2,8 +2,38 @@
 
 pragma solidity ^0.8.7;
 
+// Errors =====================================================================
+error Raffle__notSufficientFund();
+
+// Raffle Contract ############################################################
 contract Raffle {
-    constructor() {}
+    // Variables ===============================================================
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    // constructor () ===========================================================
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    // enterRaffle () ===========================================================
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__notSufficientFund();
+        }
+
+        s_players.push(payable(msg.sender));
+    }
+
+    // Getter functions ==========================================================
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayers(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
 }
 
-// ===========================================================
+// ================================================================
